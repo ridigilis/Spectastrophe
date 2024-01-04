723304efa6c1bd4a285cd97d185236c7f627f266
Ricky David Groner II <rgroner2@gmail.com>
2024-01-04 13:12:35 -0600

    Initial Commit

---
+++ Spectastrophe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spectastrophe
+//
+//  Created by Ricky David Groner II on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spectastrophe/SpectastropheApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectastropheApp.swift
+//  Spectastrophe
+//
+//  Created by Ricky David Groner II on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SpectastropheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
