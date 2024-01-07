3be249e7117b7a545c71858f29c1c374e66d64bc
Ricky David Groner II <rgroner2@gmail.com>
2024-01-06 21:34:12 -0600

    add PrelimModels with list of desired features

---
+++ Spectastrophe/PrelimModels.swift
@@ -0,0 +1,27 @@
+/*
+Let's start with the bones. The stuff and things I know will be in the game regardless of where iteration takes it.
+- There will be a PLAYER.
+- The PLAYER will have a DECK of CARDS.
+- The PLAYER will engage in ENCOUNTERS.
+- ENCOUNTERS take place in hex TILE arenas.
+- ENCOUNTERS will have ENEMIES.
+- The PLAYER and ENEMIES will take turns playing all CARDS from their HAND.
+- Playing CARDS from HAND will trigger various ACTIONS.
+- ACTIONS include movement/navigation around the ENCOUNTER arena.
+- ACTIONS also include ways to increase or decrease other resources that can be used for various reasons.
+- ACTIONS also include ways for the PLAYER to engage with ENEMIES and viceversa, like attacks and spells.
+- ACTIONS more often than not will trigger dice rolls that determine the result of the action.
+- Some CARDS that are played go to the DISCARD PILE.
+- When the DECK has been depleted, the DISCARD PILE is shuffled back into the DECK.
+- Some CARDS can also be exhausted (removed from play entirely) through various means.
+- Some CARDS, when played, are equipped by the PLAYER. Let's call them GEAR CARDS.
+- GEAR CARDS, when equipped, may add more CARDS to the PLAYER'S DECK of CARDS.
+- The PLAYER will need to defeat all ENEMIES in an ENCOUNTER in order to move onto the next ENCOUNTER.
+- ENEMIES and the PLAYER are considered defeated when their hit points reach zero or less.
+- ENEMIES may drop CARDS from their own DECK when defeated.
+- The PLAYER can pick up dropped CARDS if they are on the TILE with the drop on it.
+- The PLAYER may also be rewarded with CARDS after completing an ENCOUNTER.
+- The PLAYER will choose which ENCOUNTER to attempt next on the OVERWORLD/MAP after completing an ENCOUNTER.
+
+I think that's plenty to work with for now. Let's make some models!
+*/
