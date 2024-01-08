12bd657f8fb51e6ff5234b4687e180e3545f4f1f
Ricky David Groner II <rgroner2@gmail.com>
2024-01-08 11:50:44 -0600

    continuation of previous commit

--- Spectastrophe/ContentView.swift
+++ Spectastrophe/ContentView.swift
@@ -26,23 +26,73 @@ struct Row: View {
 }
 
 struct ContentView: View {
+    @State private var currentZoom = 0.0
+    @State private var totalZoom = 1.0
+
     var body: some View {
-        VStack {
-            Row(6)
-            Row(7)
-            Row(8)
-            Row(9)
-            Row(10)
-            Row(11)
-            Row(12)
-            Row(11)
-            Row(10)
-            Row(9)
-            Row(8)
-            Row(7)
-            Row(6)
+        HStack {
+            Spacer()
+            Spacer()
+            Spacer()
+            ZStack {
+                VStack {
+                    Row(6)
+                    Row(7)
+                    Row(8)
+                    Row(9)
+                    Row(10)
+                    Row(11)
+                    Row(12)
+                    Row(11)
+                    Row(10)
+                    Row(9)
+                    Row(8)
+                    Row(7)
+                    Row(6)
+                }
+                .aspectRatio(contentMode: .fit)
+                .scaleEffect(currentZoom + totalZoom)
+                .gesture(
+                    MagnifyGesture()
+                        .onChanged { value in
+                            currentZoom = value.magnification - 1
+                        }
+                        .onEnded { value in
+                            totalZoom += currentZoom
+                            currentZoom = 0
+                        }
+                )
+                .accessibilityZoomAction { action in
+                    if action.direction == .zoomIn {
+                        totalZoom += 1
+                    } else {
+                        totalZoom -= 1
+                    }
+                }
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                            .frame(width:120, height: 180)
+                            .foregroundColor(.brown)
+                            .shadow(radius: 12)
+
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                            .frame(width:120, height: 180)
+                            .foregroundColor(.brown)
+                            .shadow(radius: 12)
+
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                            .frame(width:120, height: 180)
+                            .foregroundColor(.brown)
+                            .shadow(radius: 12)
+                    }
+                }
+            }
+
+            Spacer()
         }
-        .aspectRatio(contentMode: .fit)
     }
 }
 
