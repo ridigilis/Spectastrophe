35bb79e82c0951352e20de5dcf32ffee083658ad
Ricky David Groner II <rgroner2@gmail.com>
2024-01-08 10:59:39 -0600

    screw around with ContentView to see what an Encounter board might look like

--- Spectastrophe/ContentView.swift
+++ Spectastrophe/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+struct Row: View {
+    var count: Int
+
+    init(_ count: Int = 0) {
+        self.count = count
+    }
+
+    var body: some View {
+        HStack {
+            ForEach(Range(0...count)) { _ in
+                Spacer().frame(width: 4)
+                Circle().fill(.gray).padding(-4)
+                Spacer().frame(width: 8)
+            }
+        }.padding(-4)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Row(6)
+            Row(7)
+            Row(8)
+            Row(9)
+            Row(10)
+            Row(11)
+            Row(12)
+            Row(11)
+            Row(10)
+            Row(9)
+            Row(8)
+            Row(7)
+            Row(6)
         }
-        .padding()
+        .aspectRatio(contentMode: .fit)
     }
 }
 
